689ef69a1a4e6bc7d134cee2a05dd8ef999acc73
44smkn <jrywm121@gmail.com>
2022-03-26 13:23:35 +0900

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,51 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {getSdk, TeamsUserBelongsQuery} from './generated/graphql'
+import {GraphQLClient} from 'graphql-request'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const organization = core.getInput('organization')
+    const username = core.getInput('username')
+    const teams = core.getMultilineInput('teams').map(t => t.trim())
+    core.info(
+      `Starting to check if ${username} belongs to ${teams} in ${organization} ...`
+    )
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const githubToken = core.getInput('token')
+    const client = new GraphQLClient('https://api.github.com/graphql', {
+      headers: {
+        authorization: `token ${githubToken}`
+      }
+    })
+    const sdk = getSdk(client)
 
-    core.setOutput('time', new Date().toTimeString())
+    let after: string | null | undefined = null
+    let response: TeamsUserBelongsQuery
+    do {
+      response = await sdk.TeamsUserBelongs({
+        first: 20,
+        after: after,
+        userLogins: [username],
+        organization: organization
+      })
+      core.debug(JSON.stringify(response))
+
+      const userBelongs = response.organization?.teams.edges?.some(e => {
+        const team = e?.node?.name
+        core.debug(`team: ${team}`)
+        return team != null && teams.includes(team)
+      })
+      if (userBelongs) {
+        core.setOutput('userBelongsToGivenTeams', true)
+        return
+      }
+      after = response.organization?.teams.pageInfo.endCursor
+    } while (response.organization?.teams.pageInfo.hasNextPage)
+
+    core.setOutput('userBelongsToGivenTeams', false)
+    if (core.getBooleanInput('failIfuserNotBelongs')) {
+      core.setFailed(`${username} doesn't belong to ${teams}`)
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
